0184ac447dcee36c50e314787ecac5722df63fb7
mathavan <astratechno88@gmail.com>
2026-09-16 22:34:16 +0530

    fix(android): align Kotlin and invalidate CI build cache

--- mobile/android/settings.gradle.kts
+++ mobile/android/settings.gradle.kts
@@ -20,7 +20,10 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    // Firebase's current Android dependencies are compiled with Kotlin 2.2
+    // metadata. Keeping the compiler at 2.1 (or restoring an old CI cache
+    // with 2.0) produces an endless stream of metadata errors at assemble.
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.3" apply false
 }
